b2f24f7eb20bd8a4897f09610329e86869076cbc
FilipOlsson99 <filip.olsson9911@gmail.com>
2020-08-06 12:51:51 +1200

    Current

---
+++ Fear Game/Assets/Scripts/Underwater.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Underwater : MonoBehaviour
+{
+
+    public float waterHeight;
+
+    public bool isUnderwater;
+    private Color Abovewatercolor;
+    private Color Underwatercolor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Abovewatercolor = new Color(0.4f, 0.4f, 0.4f, 0.5f);
+        Underwatercolor = new Color(0.15f, 0.25f, 0.3f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((transform.position.y < waterHeight) != isUnderwater)
+        {
+            isUnderwater = transform.position.y < waterHeight;
+            if (isUnderwater) SetUnderwater();
+            if (!isUnderwater) SetNormal();
+        }
+    }
+
+    void SetNormal()
+    {
+        RenderSettings.fogColor = Abovewatercolor;
+        RenderSettings.fogDensity = 0.004f;
+    }
+
+    void SetUnderwater()
+    {
+        RenderSettings.fogColor = Underwatercolor;
+        RenderSettings.fogDensity = 0.02f;
+    }
+}
