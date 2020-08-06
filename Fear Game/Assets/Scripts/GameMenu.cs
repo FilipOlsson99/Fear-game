554e980c26068196ba38f8c756e25838c9a0a63f
Jet Yong <cherryyong4075@gmail.com>
2020-08-06 14:30:40 +1200

    Auto stash before merge of "master" and "origin/master"

---
+++ Fear Game/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    public void loadlevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
+
